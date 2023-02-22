a8c423e1205705aa3dd57a2f4ce15333629daa28
lcchaverra <lucas.cordoba1510@gmail.com>
2023-02-22 12:50:51 -0500

    FlyChallenge

---
+++ src/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Header from './components/Header'
+// import MainBanner from './components/MainBanner'
+// import MainFooter from './components/Footer'
+
+const HomePage = () => {
+    return(
+        <>
+        <h2>asdasds</h2>
+        {/* <Header />
+        <MainBanner/>
+        <MainFooter/>  */}
+        </>
+    )
+}
+
+export default HomePage
