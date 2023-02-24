c98f0ec57c0a99709ba616c1c757a8a8df275004
lcchaverra <lucas.cordoba1510@gmail.com>
2023-02-24 15:02:04 -0500

    actualizacion de los estilos para el layout

--- src/components/EditVaca.jsx
+++ src/components/EditVaca.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { collection ,getDoc, updateDoc, doc} from 'firebase/firestore'
+import { collection ,getDoc, updateDoc, doc, getDocs} from 'firebase/firestore'
 import { db } from '../firebaseConfig/firebase';
 import { async } from '@firebase/util'
 import Swal from 'sweetalert2'
@@ -17,16 +17,30 @@ const EditVaca = () => {
         const [estado,setestado]= useState([])
         const [asignar, setasignar]= useState([])
 
+        // Hook para usuario
+        const [users, setUsers] = useState([]);
+
+        //referencia a la DB
+        const usersCollection = collection (db, "users");
     
+        //Funcion para mostrar todos los Docs
+        const getUsers = async () => {
+        const data = await getDocs(usersCollection)
+        const usersData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        setUsers(usersData)
+        console.log(usersData.map(user => user.Nombres))  
+        }
+        // getUsers()
+        
         const navigate = useNavigate()
         const {id} = useParams()
-
+        
         const update = async (e) => {
             e.preventDefault()
             const user = doc(db,"vacantes", id)
             const data = {codigo: codigo, cargo: cargo, descripcion: descripcion, empresa: empresa, salario: salario, estado: estado, asignar: asignar}
             await updateDoc(user,data)
-
+            
             Swal.fire({
                 position: 'top-end',
                 icon: 'success',
@@ -34,10 +48,10 @@ const EditVaca = () => {
                 showConfirmButton: false,
                 timer: 1500
             })
-
+            
             navigate('../ModuleVaca')
         }
-
+        
         const getUserById = async (id)=>{
             const user = await getDoc(doc(db,"vacantes", id))
             if(user.exists()){
@@ -50,19 +64,19 @@ const EditVaca = () => {
                 setasignar(user.data().asignar)
                 
             }
-            // ?console.log(user.data())
             else{
                 console.log('No existe')
             }
         }
-
+        
         useEffect( ()=>{
             getUserById(id)
+            setUsers()
         },[])
-
+        
         return (
             <>
-            <h2 className="title">Edicion de Vacantes</h2>
+            <h2 className="title">Aplicacion de Vacantes</h2>
             <div className="main-container">
                 <form onSubmit={update}>
                     <div className="grid-form-container">
@@ -102,10 +116,36 @@ const EditVaca = () => {
                             <label >asignar  </label>
                             <input value={asignar} onChange={(e)=>setasignar(e.target.value)} type="text"></input>
                         </div>
+
+                        {/* usersData.map(user => user.Nombres) */}
+                        {/* <select>
+                        {usersData.map((user) => (
+                              <option className='option-2' value={user.Nombres}>{user.Nombres}</option>  
+                             ))}
+                        </select> */}
+                            {/* // <tr key={user.id}>
+                            //     <td>{user.TipoDocument}</td>
+                            //     <td>{user.Documento}</td>
+                            //     <td>{user.Nombres}</td>
+                            //     <td>{user.Apellidos}</td>
+                            //     <td>{user.Fecha}</td>
+                            //     <td>{user.Profesion}</td>
+                            //     <td>{user.Aspiracion}</td>
+                            //     <td>{user.Correo}</td>
+                            // </tr> */}
+                            
+
+                        {/* <div className="form-item">
+                        <label >asignar Aspirante</label>
+                            <select className='select-2' multiple value={estado} onChange={(e) => setestado(Array.from(e.target.selectedOptions, (option) => option.value))}>
+                                <option className='option-2' value="Activo" >Activo</option>
+                            </select>
+                        </div> */}
+
                         
                     </div>
                         <div className="form-item">
-                            <button className="button" type="submit">Modificar Vacante</button>
+                            <button className="button" type="submit">Actualizar Estado de Vacante</button>
                         </div>
                     </form>
                         <div className="section-divider"></div>
